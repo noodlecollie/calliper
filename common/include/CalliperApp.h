ae3db42ff0a8ffb3ed9e5ff9556f44324a416310
x6herbius <jonathan.poncelet@talk21.com>
2015-09-05 18:25:47 +0100

    First experiments with menu bar

--- common/include/CalliperApp.h
+++ common/include/CalliperApp.h
@@ -3,13 +3,34 @@
 
 #include "PolycodeView.h"
 #include "Polycode.h"
+#include "PolycodeUI.h"
+
+using namespace Polycode;
+
+// This global pointer can be used to access the generic app.
+class CalliperApp;
+extern CalliperApp* globalApp;
 
 class CalliperApp
 {
 public:
+	// View setup defaults.
+	static const int RESOLUTION_X_DEFAULT = 640;
+	static const int RESOLUTION_Y_DEFAULT = 480;
+	static const int FRAMERATE_DEFAULT = 60;
+
+	// System identifiers.
+	enum SystemIdentifier
+	{
+		SystemWindows = 0,
+		SystemMac,
+		SystemUnix
+	};
+
+public:
 	// This constructor caters to differing platforms. Pass in the view and the core pointer that
 	// need to be initialised on the given platform.
-	CalliperApp(Core** core, Polycode::PolycodeView* view);
+	CalliperApp(Core** core, PolycodeView* view);
 	
 	// Destructor should be called by deleting the app from the platform-specific destructor.
 	~CalliperApp();
@@ -17,18 +38,21 @@ public:
 	// Should be called directly from the platform-specific Update() function.
 	bool Update();
 
-	// View setup defaults.
-public:
-	static const int RESOLUTION_X_DEFAULT = 640;
-	static const int RESOLUTION_Y_DEFAULT = 480;
-	static const int FRAMERATE_DEFAULT = 60;
+	SystemIdentifier GetSystemIdentifier() const;
+	String GetSystemStringName() const;
+
+	Scene* GetScreen() const;
 
 private:
 	void PlatformSpecificInitialisation(Core** core, PolycodeView* view);
 	void InitialiseResources();
+	void InitialiseScreen();
+	void InitialiseMenuBar();
 
 private:
-	Core* m_pCore;	// Also cache our core object. This should be alive as long as we are.
+	Core* m_pCore;			// Also cache our core object. This should be alive as long as we are.
+	Scene* m_pScreen;		// 2D scene that holds the UI etc.
+	UIMenuBar* m_pMenuBar;	// Application menu bar.
 };
 
 #endif // CALLIPER_APP_H
--- common/src/CalliperApp.cpp
+++ common/src/CalliperApp.cpp
@@ -2,9 +2,22 @@
 
 using namespace Polycode;
 
+CalliperApp* globalApp = NULL;
+UIGlobalMenu* globalMenu = NULL;
+
 CalliperApp::CalliperApp(Core** core, PolycodeView* view)
 {
+	// Create the core for this platform.
 	PlatformSpecificInitialisation(core, view);
+
+	// Load resources that are needed on startup.
+	InitialiseResources();
+
+	// Set up the screen scene.
+	InitialiseScreen();
+
+	// Set up the menu bar.
+	InitialiseMenuBar();
 }
 
 CalliperApp::~CalliperApp()
@@ -19,7 +32,7 @@ bool CalliperApp::Update()
 
 void CalliperApp::PlatformSpecificInitialisation(Core** core, PolycodeView* view)
 {
-		// Do different things depending on our platform.
+	// Do different things depending on our platform.
 #if PLATFORM == PLATFORM_WINDOWS
 	*core = new Win32Core(view, RESOLUTION_X_DEFAULT, RESOLUTION_Y_DEFAULT, false, false, 0, 0, FRAMERATE_DEFAULT);	  
 #elif PLATFORM == PLATFORM_MAC
@@ -31,11 +44,100 @@ void CalliperApp::PlatformSpecificInitialisation(Core** core, PolycodeView* view
 #endif
 
 	m_pCore = *core;
+
+	// Set us as the global application.
+	// Make sure this hasn't been done yet!
+	assert(!globalApp);
+	globalApp = this;
 }
 
 void CalliperApp::InitialiseResources()
 {
+	// .pak files are really just renamed ZIPs. They can be opened with the Windows ZIP viewer.
+	// addArchive() gets the specified file or path and adds it as a search path. The content itself is not loaded.
+	// We do this on the .pak files, but you can also pass an actual folder to this function.
+	// addDirResource() then looks at all of the search paths given so far and loads actual resources.
+
+	// Eg. addArchive("default.pak") makes the contents within default.pak visible to the resource manager.
+	// default.pak has a root "default" directory which is now visible because of the above call.
+	// addDirResource("default") loads all the contents recursively from this folder.
+
 	// Load default content.
 	CoreServices::getInstance()->getResourceManager()->addArchive("default.pak");
 	CoreServices::getInstance()->getResourceManager()->addDirResource("default", false);
+
+	// Load HDR shaders etc.
+	CoreServices::getInstance()->getResourceManager()->addArchive("hdr.pak");
+	CoreServices::getInstance()->getResourceManager()->addDirResource("hdr");
+
+	// Load UI content.
+	CoreServices::getInstance()->getResourceManager()->addArchive("UIThemes.pak");
+	CoreServices::getInstance()->getConfig()->loadConfig("Polycode", "UIThemes/default/theme.xml");
+
+	// Load Calliper's native content.
+	Logger::log("Adding native content...\n");
+	CoreServices::getInstance()->getResourceManager()->addArchive("native.pak");
+	CoreServices::getInstance()->getResourceManager()->addDirResource("native");
+}
+
+CalliperApp::SystemIdentifier CalliperApp::GetSystemIdentifier() const
+{
+#if PLATFORM == PLATFORM_WINDOWS
+	return SystemWindows;
+#elif PLATFORM == PLATFORM_MAC
+	return SystemMac;
+#else
+	return SystemUnix;
+#endif
+}
+
+String CalliperApp::GetSystemStringName() const
+{
+#if PLATFORM == PLATFORM_WINDOWS
+	return "Windows";
+#elif PLATFORM == PLATFORM_MAC
+	return "Mac";
+#else
+	return "Unix";
+#endif
+}
+
+Scene* CalliperApp::GetScreen() const
+{
+	return m_pScreen;
+}
+
+void CalliperApp::InitialiseScreen()
+{
+	// We want 2D top left for standard UI co-ordinates.
+	m_pScreen = new Scene(Scene::SCENE_2D_TOPLEFT);
+
+	// Don't do any visibility culling for the screen.
+	m_pScreen->doVisibilityChecking(false);
+	m_pScreen->getDefaultCamera()->frustumCulling = false;
+
+	// We use nearest neighbour filtering to keep fonts crisp.
+	CoreServices::getInstance()->getRenderer()->setTextureFilteringMode(Renderer::TEX_FILTERING_NEAREST);
+
+	// Process input events!
+	m_pScreen->rootEntity.processInputEvents = true;
+}
+
+void CalliperApp::InitialiseMenuBar()
+{
+	assert(!globalMenu);
+
+	// Create the global menu.
+	// TODO: What exactly is this and why is it needed?
+	globalMenu = new UIGlobalMenu();
+	UITextInput::setMenuSingleton(globalMenu);
+	m_pScreen->addChild(globalMenu);
+
+	// Create the menu bar.
+	m_pMenuBar = new UIMenuBar(300, globalMenu);
+
+	UIMenuBarEntry *fileEntry = m_pMenuBar->addMenuBarEntry("File");
+	fileEntry->addItem("New File", "new_file", KEY_n);
+
+	m_pScreen->addChild(m_pMenuBar);
 }
