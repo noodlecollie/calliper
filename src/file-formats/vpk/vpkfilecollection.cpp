4325b602d92cea8ff476e7bf16109b8b19fbb637
Vesper <jonathan.poncelet@talk21.com>
2016-11-16 07:38:49 +0000

    Put together VPK file collection

--- src/file-formats/vpk/vpkfilecollection.cpp
+++ src/file-formats/vpk/vpkfilecollection.cpp
@@ -6,4 +6,42 @@ namespace FileFormats
     {
 
     }
+
+    VPKFileCollection::~VPKFileCollection()
+    {
+        qDeleteAll(m_FilesContainingExtensions.values());
+        m_FilesContainingExtensions.clear();
+    }
+
+    void VPKFileCollection::addFile(const VPKFilePointer &file)
+    {
+        m_Files.append(file);
+        processAddFile(file);
+    }
+
+    void VPKFileCollection::processAddFile(const VPKFilePointer &file)
+    {
+        QStringList extensions = file->index().extensions();
+
+        foreach ( const QString& ext, extensions )
+        {
+            QSet<VPKFilePointer>* set = m_FilesContainingExtensions.value(ext, nullptr);
+
+            if ( !set )
+            {
+                set = new QSet<VPKFilePointer>();
+                m_FilesContainingExtensions.insert(ext, set);
+            }
+
+            set->insert(file);
+        }
+    }
+
+    QSet<VPKFilePointer> VPKFileCollection::filesContainingExtension(const QString &extension) const
+    {
+        if ( !m_FilesContainingExtensions.contains(extension) )
+            return QSet<VPKFilePointer>();
+
+        return *m_FilesContainingExtensions.value(extension);
+    }
 }
--- src/file-formats/vpk/vpkfilecollection.h
+++ src/file-formats/vpk/vpkfilecollection.h
@@ -2,13 +2,33 @@
 #define VPKFILECOLLECTION_H
 
 #include "file-formats_global.h"
+#include <QSharedPointer>
+#include "vpkfile.h"
+#include <QList>
+#include <QHash>
+#include <QSet>
 
 namespace FileFormats
 {
+    typedef QSharedPointer<VPKFile> VPKFilePointer;
+
     class VPKFileCollection
     {
     public:
         VPKFileCollection();
+        ~VPKFileCollection();
+
+        void addFile(const VPKFilePointer& file);
+
+        QSet<VPKFilePointer> filesContainingExtension(const QString& extension) const;
+
+    private:
+        typedef QHash<QString, QSet<VPKFilePointer>* > ExtensionTable;
+
+        void processAddFile(const VPKFilePointer& file);
+
+        QList<VPKFilePointer> m_Files;
+        ExtensionTable m_FilesContainingExtensions;
     };
 }
 
