3e551c50114c74875f9c8d29425a0d8293aca686
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2017-02-03 10:57:36 +0000

    Fixed include script to work on windows

--- src/create-module-headers.py
+++ src/create-module-headers.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python
 
-import sys, os, re
+import sys, os, re, getopt, posixpath
 from glob import glob
 
 def checkVersion():
 	if sys.hexversion >= 0x03000000:
 		print("This script does not support Python 3. Please run using Python 2.")
-		exit()
+		sys.exit(1)
 
 def childDirs(parentDir):
-	return [x.strip('/') for x in glob('*/')]
+	return [x.rstrip(os.sep) for x in glob('*/')]
 
 def openFile(filename, mode):
 	try:
@@ -33,6 +33,8 @@ def moduleExportDef(moduleName):
 class MainClass:
 	rootDir = ""
 	currentModule = ""
+	verbose = False
+	overwriteExisting = False
 
 	globalHeaderPath = ""
 	moduleExportDef = ""
@@ -40,17 +42,42 @@ class MainClass:
 	targetFilePath = ""
 	targetFileContents = ""
 
-	def __init__(self):
+	def __init__(self, argv):
+		self.vlog("Running script using Python:\n" + sys.version + "\n")
+	
 		self.rootDir = os.path.abspath(".")
+		
+		try:
+			opts, args = getopt.getopt(argv, "vx")
+		except getopt.GetoptError as err:
+			print("Usage:")
+			print("create-module-headers.py: -v -x")
+			print("  -v: Print verbose output.")
+			print("  -x: Overwrite include files that already exist.")
+			sys.exit(1)
+		
+		for opt, arg in opts:
+			if opt == "-v":
+				self.verbose = True
+				print("Verbose mode enabled.")
+		
+			elif opt == "-x":
+				self.overwriteExisting = True
+				print("Existing headers will be overwritten.")
+	
+	def vlog(self, msg):
+		if self.verbose:
+			print(msg)
 
 	def moduleGlobalHeaderName(self):
 		return self.currentModule + "_global.h"
 
 	def moduleDirPath(self):
-		return self.rootDir + "/" + self.currentModule
+		return os.path.normpath(self.rootDir + "/" + self.currentModule)
 
 	def setModuleVars(self):
-		self.globalHeaderPath = self.rootDir + "/" + self.currentModule + "/" + self.moduleGlobalHeaderName()
+		self.globalHeaderPath = os.path.normpath(self.rootDir + "/" + self.currentModule +
+		"/" + self.moduleGlobalHeaderName())
 		self.moduleExportDef = moduleExportDef(self.currentModule)
 
 	def fileContainsContainsModuleExport(self, contents):
@@ -74,12 +101,22 @@ class MainClass:
 		relativePath = self.getRelativeFilePath(absolutePath)
 		relativeDir = os.path.dirname(relativePath)
 
-		pathFromIncludeDir = "../../" + relativeDir + "/" + fileName
-		self.targetFileContents = '#include "' + pathFromIncludeDir + '"'
-		self.targetFilePath = self.rootDir + "/include/" + self.currentModule + "/" + fileName
+		pathFromIncludeDir = os.path.normpath("../../" + relativeDir + "/" + fileName)
+		self.targetFileContents = '#include "' + pathFromIncludeDir.replace("\\", "/") + '"'
+		self.targetFilePath = os.path.normpath(self.rootDir + "/include/"
+		+ self.currentModule + "/" + fileName)
 		
-		if not os.path.exists(self.rootDir + "/include/" + self.currentModule):
-			os.makedirs(self.rootDir + "/include/" + self.currentModule)
+		self.vlog("Ready to create include file:")
+		self.vlog("  File: '" + self.targetFilePath + "'")
+		self.vlog("  Contents: '" + self.targetFileContents + "'")
+		
+		modulePath = os.path.normpath(self.rootDir + "/include/" + self.currentModule)
+		if not os.path.exists(modulePath):
+			os.makedirs(modulePath)
+			
+		if not self.overwriteExisting and os.path.isfile(self.targetFilePath):
+			print("Include file '" + self.targetFilePath + "' already exists, skipping.")
+			return
 
 		print("Creating include for '" + self.targetFilePath + "'")
 		self.writeIncludeFile()
@@ -94,7 +131,7 @@ class MainClass:
 				if not file.lower().endswith(('.h')):
 					continue
 
-				absoluteFilePath = root + "/" + file
+				absoluteFilePath = os.path.normpath(root + "/" + file)
 
 				f = openFile(absoluteFilePath, 'r')
 				if f is None:
@@ -113,7 +150,9 @@ class MainClass:
 		self.setModuleVars()
 
 		print("Processing directory " + self.currentModule + "...")
-
+		
+		self.vlog("Attempting to find global header '" + self.globalHeaderPath + "'...")
+		
 		if not os.path.isfile(self.globalHeaderPath):
 			print("Could not find global header for module, skipping.")
 			return
@@ -121,18 +160,17 @@ class MainClass:
 		self.walkDirs()
 
 	def process(self):
-		if not os.path.exists("./include"):
+		if not os.path.exists(os.path.normpath("./include")):
 			os.makedirs("./include")
 
 		for subdir in childDirs('.'):
 			if subdir != "include":
 				self.processModule(subdir)
 
-def main():
+def main(argv):
 	checkVersion()
-	print("Running script using Python:\n" + sys.version + "\n")
 
-	prog = MainClass()
+	prog = MainClass(argv)
 	prog.process()
 
 # ======================================
@@ -140,4 +178,4 @@ def main():
 # ======================================
 
 if __name__ == '__main__':
-  main()
+  main(sys.argv[1:])
