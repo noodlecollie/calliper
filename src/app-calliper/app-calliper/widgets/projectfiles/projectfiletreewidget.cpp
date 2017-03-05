e7bd2c161b25b1e6e989dcb8879aa82c259f3f0f
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2017-03-05 17:16:24 +0000

    Damn, missed one

--- src/app-calliper/app-calliper/widgets/projectfiles/projectfiletreewidget.cpp
+++ src/app-calliper/app-calliper/widgets/projectfiles/projectfiletreewidget.cpp
@@ -76,6 +76,7 @@ namespace AppCalliper
         item->addChild(child);
         m_ItemsByPath.insert(localPath, child);
 
+        // TODO: Fix
         if ( shouldExpandItem )
         {
             expandItem(item);
